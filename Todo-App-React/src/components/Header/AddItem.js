03b30e557e49dbb303a847ff6585209d36cea935
Jacob Marshall <jacob@omg.lol>
2021-10-07 15:50:14 +0100

    Don't use forms

--- Todo-App-React/src/components/Header/AddItem.js
+++ Todo-App-React/src/components/Header/AddItem.js
@@ -3,34 +3,40 @@ import { useState } from "react";
 function AddItem(p) {
   const [item, setItem] = useState("");
   const { takeItem } = p;
-  
-  function submit(e) {
-    e.preventDefault()
-    taskAdded()
+
+  function listenForEnter(e) {
+    if (e.key === "Enter") {
+      taskAdded();
+    }
   }
 
   const taskAdded = () => {
     if (item.length > 1) {
-      takeItem(item)
-      setItem("")
+      takeItem(item);
+      setItem("");
+    } else {
+      alert("Must Enter Something");
     }
-    else {
-      alert("Must Enter Something"      )
-    }
-    }
+  };
 
-    return (
-            <>
-  <div>
-  <h2 style={{margin:"5px"}} >My To Do List</h2>
-  <form onSubmit={submit}>
-  <input type="text" id="myInput" placeholder="Title..." value={item} onChange = {(e) => setItem(e.target.value)}  />
-  <button type="submit" className="addBtn" onClick={taskAdded}>Add</button>
-  </form>
-</div>
-
-            </>
-    )
+  return (
+    <>
+      <div>
+        <h2 style={{ margin: "5px" }}>My To Do List</h2>
+        <input
+          type="text"
+          id="myInput"
+          placeholder="Title..."
+          value={item}
+          onChange={(e) => setItem(e.target.value)}
+          onKeyPress={this.listenForEnter}
+        />
+        <button className="addBtn" onClick={taskAdded}>
+          Add
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default AddItem;
