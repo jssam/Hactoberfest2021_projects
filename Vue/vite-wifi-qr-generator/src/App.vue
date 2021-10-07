ea1a5f6481774b6e2732c8093c261a831c57f12e
Mihai Andrei <mihai.andrei@modex.tech>
2021-10-07 21:19:29 +0300

    created a vue folder and added wifi-qrcode-generator

---
+++ Vue/vite-wifi-qr-generator/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="w-screen h-screen flex justify-center items-center bg-blue-800">
+    <FormGenerator @onSubmit="onSubmit" v-if="!text" />
+    <QRCode v-else :text="text" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "@vue/reactivity";
+import FormGenerator from "./components/FormGenerator.vue";
+import QRCode from "./components/QRCode.vue";
+
+const text = ref();
+
+const onSubmit = ({ ssid, password, network_encryption }) => {
+  text.value = `WIFI:T:${network_encryption.value};S:${ssid.value};P:${password.value};;`;
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
