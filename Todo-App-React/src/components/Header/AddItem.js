4361f531a0f9e53c4441417a0f5decf259267a69
Jacob Marshall <jacob@omg.lol>
2021-10-07 14:56:12 +0100

    Update AddItem.js

--- Todo-App-React/src/components/Header/AddItem.js
+++ Todo-App-React/src/components/Header/AddItem.js
@@ -3,6 +3,11 @@ import { useState } from "react";
 function AddItem(p) {
   const [item, setItem] = useState("");
   const { takeItem } = p;
+  
+  function submit(e) {
+    e.preventDefault()
+    taskAdded()
+  }
 
   const taskAdded = () => {
     if (item.length > 1) {
@@ -18,7 +23,9 @@ function AddItem(p) {
             <>
   <div>
   <h2 style={{margin:"5px"}} >My To Do List</h2>
+  <form onSubmit={submit}>
   <input type="text" id="myInput" placeholder="Title..." value={item} onChange = {(e) => setItem(e.target.value)}  />
+  </form>
   <span  className="addBtn" onClick = {taskAdded}>Add</span>
 </div>
 
