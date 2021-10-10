abf39e52e63315facd59c70ecb90c6b4354185fa
chandradharrao <chandradhar.rao@gmail.com>
2021-10-11 01:05:13 +0530

    Added two leetcode problem solution

---
+++ Leetcode Questions/EvaluateReversePolishNottion.py
@@ -0,0 +1,12 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stk = []
+        for i in tokens:
+            if i=="+" or i=="/" or i=="*" or i=="-":
+                right = stk.pop()
+                left = stk.pop()
+                res = int(eval(f"{left}{i}{right}"))
+                stk.append(res)
+            else:
+                stk.append(i)
+        return stk[0]
