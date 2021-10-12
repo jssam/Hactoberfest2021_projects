60649be56bae52df225a2dd6c2ce548181056023
Sanskar Bansal <32870271+sanskarbansal@users.noreply.github.com>
2021-10-12 23:19:25 +0530

    Create reverseLinkedListRecursive.cpp
    added Linked List Reverse Program recursive

---
+++ CPP/reverseLinkedListRecursive.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+
+pair<Node*, Node*> helper(Node *head){
+    if(head -> next == NULL){
+        return make_pair(head, head); 
+    }
+    pair<Node*, Node*> smallOutput = helper(head -> next); 
+    head -> next = NULL; 
+    smallOutput.second -> next = head; 
+    smallOutput.second = head; 
+    return smallOutput; 
+}
+
+
+Node *reverseLinkedListRec(Node *head)
+{
+    if(head == NULL) return NULL; 
+    return helper(head).first; 
+}
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newnode = new Node(data);
+		if (head == NULL)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail->next = newnode;
+			tail = newnode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+void print(Node *head)
+{
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+
+	while(t--)
+	{
+		Node *head = takeinput();
+		head = reverseLinkedListRec(head);
+		print(head);
+	}
+
+	return 0;
+}
